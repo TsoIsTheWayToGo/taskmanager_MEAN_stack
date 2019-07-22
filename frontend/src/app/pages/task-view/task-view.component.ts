61d44b8186f111d9604121469167071adde3bcdb
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-07-22 12:09:46 -0700

    Connect UI with backend to display content dynamically

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -5,30 +5,25 @@ import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-task-view',
   templateUrl: './task-view.component.html',
-  styleUrls: ['./task-view.component.scss']
+  styleUrls: ['./task-view.component.scss'],
 })
 export class TaskViewComponent implements OnInit {
-
   lists: any[];
 
-  task: any[];
-  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+  tasks: any[];
+  constructor(private taskService: TaskService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      console.log(params)
-      
-      this.taskService.getTasks(params._id).subscribe((tasks: any[])=> {
-        console.log(tasks);
-        
-      })
-    }
-    )
+      console.log(params);
 
-    this.taskService.getList().subscribe((lists: any[])=> {
+      this.taskService.getTasks(params.listId).subscribe((tasks: any[]) => {
+        this.tasks = tasks;
+      });
+    });
+
+    this.taskService.getList().subscribe((lists: any[]) => {
       this.lists = lists;
-    })
+    });
   }
-
-  
 }
