e4cb570d3484c2eb120ffad6e2484bf7f582da09
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-07-22 20:23:30 -0700

    Create authenitcate middleware to verify jwt and protect api routes

--- frontend/src/app/web-req.interceptor.service.spec.ts
+++ frontend/src/app/web-req.interceptor.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { WebReq.InterceptorService } from './web-req.interceptor';
+import { WebReqInterceptor } from './web-req.interceptor';
 
 describe('WebReq.InterceptorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: WebReq.InterceptorService = TestBed.get(WebReq.InterceptorService);
+    const service: WebReqInterceptor = TestBed.get(WebReqInterceptor);
     expect(service).toBeTruthy();
   });
 });
