d110cc4454715917a0e8c56ce1369bdb2e352990
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-07-22 22:43:01 -0700

    Add edit and delete buttons for tasks and lists

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -12,7 +12,10 @@ export class TaskService {
   createList(title: string) {
     return this.webReqService.post('lists', { title });
   }
-  
+  updateList(id: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${id}`, { title });
+  }
   getList(){
     return this.webReqService.get('lists');
   }
@@ -30,4 +33,17 @@ export class TaskService {
       completed: !task.completed
     })
   }
+
+  deleteList(id: string) {
+    return this.webReqService.delete(`lists/${id}`);
+  }
+
+  deleteTask(listId: string, taskId: string) {
+    return this.webReqService.delete(`lists/${listId}/tasks/${taskId}`);
+  }
+  updateTask(listId: string, taskId: string, title: string) {
+   
+    return this.webReqService.patch(`lists/${listId}/tasks/${taskId}`, { title });
+  }
+
 }
